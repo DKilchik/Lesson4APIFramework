79639fb02573e869600b945592e4f04b446abd92
Dmitri Cilcic <dmitri.cilcic@orange.com>
2024-05-17 22:02:52 +0300

    attachment

--- src/test/java/tests/TestReqres.java
+++ src/test/java/tests/TestReqres.java
@@ -1,11 +1,27 @@
 package tests;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.testng.AllureTestNg;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 
 public class TestReqres {
 
     @Test
-    public void testTestNG(){
+    public void testListUser(){
 
+        String url = "https://reqres.in/api/users?page=2";
+
+        Allure.addAttachment("URL: ", url);
+
+        Response resp = get(url);
+
+        Allure.addAttachment("response body", resp.body().prettyPrint());
+        Allure.addAttachment("status code", String.valueOf(resp.statusCode()));
+
+        resp.then().assertThat().statusCode(200);
     }
 }
